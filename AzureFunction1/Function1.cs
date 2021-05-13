dc29d6cbdadbfbc5b3315c11ca766819aa3b4cb3
Adam Kadzban <adam.kadzban@relativity.com>
2021-05-13 17:00:43 -0500

    Adding new feature

--- AzureFunction1/Function1.cs
+++ AzureFunction1/Function1.cs
@@ -27,7 +27,7 @@ namespace AzureFunction1
 
 			string responseMessage = string.IsNullOrEmpty(name)
 				? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-				: $"Hello, {name}. This HTTP triggered function executed successfully.";
+				: $"Hello, {name}. This HTTP triggered function executed successfully. Look at this cool new feature!";
 
 			return new OkObjectResult(responseMessage);
 		}
